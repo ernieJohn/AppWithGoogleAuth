d0806d7f250eb6e2c20db6b8278d97d2773c294c
ernieJohn <erniejohndbernabat@gmail.com>
2025-02-28 16:56:31 -0800

    login view was created

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -35,6 +35,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::registerView(function () {
             return view('auth.register');
         });
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
 
 
         RateLimiter::for('login', function (Request $request) {
